5cabafd7ef3a5098245c5d82096ab32e6aec3fca
胡一凯 <2096136603@qq.com>
2019-10-26 14:12:03 +0800

    练习题

---
+++ 练习题/练习题/test.c
@@ -0,0 +1,61 @@
+////////////////////////反转一个链表
+//#include<stdio.h>
+//int main()
+//{
+//	struct ListNode* reverseList(struct ListNode* head){
+//
+//		if (head == NULL || head->next == NULL)
+//			return head;
+//		struct ListNode* p = head;
+//		head->next = NULL;
+//
+//		while (p != NULL)
+//		{
+//			struct ListNode* q = p->next;
+//			p->next = head;
+//			head = p;
+//			p = q;
+//		}
+//		return head;
+//	}
+//	return 0;
+//}
+///////////////////////////////////移除链表元素
+//#include<stdio.h>
+//int main()
+//{
+//	struct ListNode* removeElements(struct ListNode* head, int val)
+//	{
+//		if (head == NULL)
+//			return NULL;
+//
+//
+//		struct ListNode *p = head;
+//		struct ListNode *prev = NULL;
+//
+//		while (p != NULL)
+//		{
+//			if (p->val == val)
+//			{
+//				if (prev == NULL)
+//				{
+//					head = head->next;
+//					free(p);
+//					p = head;
+//				}
+//				else
+//				{
+//					prev->next = p->next;
+//					free(p);
+//					p = prev->next;
+//				}
+//			}
+//			else
+//			{
+//				prev = p;
+//				p = p->next;
+//			}
+//		}
+//		return head;
+//	}
+//}
