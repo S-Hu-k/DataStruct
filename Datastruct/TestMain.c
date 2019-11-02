9248151ee5a50b8d111da01d09d72f81fb00a77f
胡一凯 <2096136603@qq.com>
2019-11-02 17:19:30 +0800

    o练习题

--- Datastruct/TestMain.c
+++ Datastruct/TestMain.c
@@ -9,7 +9,63 @@
 //#include"stack.h"
 //#include"queue.h"
 #include"bintree.h"
+int main()
+{
+	//char *str = "ABC##DE##F##G#H##";
+	//char *str1 = "ABC##D#F##GE##H##";
+	char *vlr = "ABCDEFGH";
+	char *lvr = "CBEDFAGH";
+	char *lrv = "CEFDBHGA";
+	int n = strlen(lvr);
+	int i = 0, j = 0;
+	BinTree bt, bt1;
+	BinTreeInit(&bt);
+	BinTreeInit(&bt1);
+	//BinTreeCreate(&bt);
+	//BinTreeCreateByStr(&bt, str, &i);
+	//BinTreeCreateByStr(&bt1, str1, &j);
+	BinTreeCreateBy_VLR_LVR(&bt, vlr, lvr, n);
+	//BinTreeCreateBy_LVR_LRV(&bt, lvr, lrv, n);
 
+	printf("VLR   :");
+	PreOrder(&bt);
+	printf("\n");
+	printf("VLR NR:");
+	PreOrderNoR(&bt);
+	printf("\n");
+	printf("\n");
+
+	//printf("LVR   :");
+	////InOrder(&bt);
+	//printf("\n");
+	//printf("LVR NR:");
+	////InOrderNoR(&bt);
+	//printf("\n");
+	//printf("\n");
+
+	//printf("LRV   :");
+	////PostOrder(&bt);
+	//printf("\n");
+	//printf("LRV NR:");
+	////PostOrderNoR(&bt);
+	//printf("\n");
+	//printf("\n");
+
+
+	//printf("Level:");
+	//LevelOrder(&bt);
+	//printf("\n");
+	//printf("Size = %d\n", Size(&bt));
+	//printf("Hegith = %d\n", Height(&bt));
+	//BinTreeNode *p = Find(&bt, 'B');
+	//p = Parent(&bt, 'H');
+
+
+	////Clone(&bt, &bt1);
+	//bool flag = Equal(&bt, &bt1);
+	return 0;
+}
+/*
 int main()
 {
 	//char *str = "ABC##DE##F##G#H##";
@@ -17,8 +73,8 @@ int main()
 	char *vlr = "ABCDEFGH";
 	char *lvr = "CBEDFAGH";
 
-	/*char *vlr = "ABCDEFGH";
-	char *lvr = "CBEDFAGH";*/
+	//char *vlr = "ABCDEFGH";
+	//char *lvr = "CBEDFAGH";
 	int n = strlen(lvr);
 	int i = 0,j=0;
 	BinTree bt;
@@ -30,10 +86,12 @@ int main()
     //BinTreeCreate(&bt);
     //BinTreeCreateByStr( &bt, str, &i);
     //BinTreeCreateByStr(&bt1, str, &j);
+
 	printf("_VLR_LVR:");
 	BinTreeCreateBy_VLR_LVR(&bt, vlr, lvr, n);
 	printf("\n");
 	
+
 	printf("VLR:");
 	PreOrder(&bt);
 	printf("\n");
@@ -59,7 +117,7 @@ int main()
 	bool flag = Equal(&bt, &bt1);
 	return 0;
 }
-
+*/
 
 
 
--- Datastruct/bintree.h
+++ Datastruct/bintree.h
@@ -343,8 +343,24 @@ void _InOrderNoR(BinTreeNode *t);
 void InOrderNoR(BinTree *t);
 void _PostOrderNoR(BinTreeNode *t);
 void PostOrderNoR(BinTree *t);
-
-
+BinTreeNode* _BinTreeCreateBy_VLR_LVR(char *vlr, char *lvr, int n)
+{
+	if (n == 0)
+		return NULL;
+	int k = 0;
+	while (lvr[k] != vlr[0])
+		k++;
+	BinTreeNode *t = (BinTreeNode*)malloc(sizeof(BinTreeNode));
+	t->data = lvr[k];
+	t->leftChild = _BinTreeCreateBy_VLR_LVR(vlr + 1, lvr, k);
+	t->rightChild = _BinTreeCreateBy_VLR_LVR(vlr + k + 1, lvr + k + 1, n - k - 1);
+	return t;
+}
+void BinTreeCreateBy_VLR_LVR(BinTree *t, char *vlr, char *lvr, int n)
+{
+	t->root = _BinTreeCreateBy_VLR_LVR(vlr, lvr, n);
+}
+/*
 BinTreeNode* _BinTreeCreateBy_VLR_LVR(char*vlr, char*lvr, int n)
 {	
 	if (n == 0)
