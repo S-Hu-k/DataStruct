0e36ff419560c72c6cf063f51c4ed68163c93038
胡一凯 <2096136603@qq.com>
2019-11-07 22:35:58 +0800

    排序

--- Datastruct/TestMain.c
+++ Datastruct/TestMain.c
@@ -9,13 +9,38 @@
 //#include"stack.h"
 //#include"queue.h"
 //#include"bintree.h"
-#include"sort.h"
-int main()
+//#include"sort.h"
+#include"heap.h"
+void main()
 {
-	int ar[]= { 5, 6, 2, 3, 4,3 , 8, 9, 7, 5, 1 };
+	int ar[] = { 27, 15, 19, 18, 28, 34, 65, 49, 25, 37 };
+	int n = sizeof(ar) / sizeof(int);
+	Heap hp;
+	int i;
+	HeapInit(&hp, n);
 
-	return 0;
+	for (i = 0; i<n; ++i)
+	{
+		HeapInsert(&hp, ar[i]);
+	}
+	HeapShow(&hp);
+
+	HeapRemove(&hp);
+	HeapShow(&hp);
+
+	HeapRemove(&hp);
+	HeapShow(&hp);
+
+	HeapDestroy(&hp);
 }
+
+
+//int main()
+//{
+//	int ar[]= { 5, 6, 2, 3, 4,3 , 8, 9, 7, 5, 1 };
+//
+//	return 0;
+//}
 /*
 int main()
 {
